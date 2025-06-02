a40724deb8fb929be271d0ddda498d9dd4a49dda
anandafadillah <anandafadillah4@gmail.com>
2025-06-02 07:56:20 +0700

    tailwind portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["index.html"],
+  theme: {
+     container :{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        "primary" : "#ec4899",
+        "secondary" : "#94a3b8",
+        "dark" : "#020617",
+      },
+      screens : {
+        '2xl' : '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
+
