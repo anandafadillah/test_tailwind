78e2331cf736c268ba4338573ab69c355e23f229
anandafadillah <anandafadillah4@gmail.com>
2025-06-05 09:57:17 +0700

    ubah nama

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
- /** @type {import('tailwindcss').Config} */
- export default {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ["index.html"],
   theme: {
-     container :{
+    container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        "primary" : "#ec4899",
-        "secondary" : "#94a3b8",
-        "dark" : "#020617",
+        primary: "#ec4899",
+        secondary: "#94a3b8",
+        dark: "#020617",
       },
-      screens : {
-        '2xl' : '1320px'
-      }
+      screens: {
+        "2xl": "1320px",
+      },
     },
   },
   plugins: [],
-}
-
+};
